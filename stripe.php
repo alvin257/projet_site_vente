cc5c8ac25d296eb4df09fae9d49f7bdcf465acdb
alvin257 <143034292+alvin257@users.noreply.github.com>
2023-12-28 15:20:32 +0100

    Delete stripe.php stripe1.php

--- stripe.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('vendor/autoload.php'); // Assurez-vous que le chemin est correct
-
-\Stripe\Stripe::setApiKey('pk_test_51OFx9tAxKaki6GC7ei92D3g6sfTrhJrpI0dIL1zpXLUkVib6wIgnDBAMyBXeFINPuzbIyzq5a01J6JREqspwx2uR00pnkbjtHX');
-
-$stripe = new \Stripe\StripeClient('sk_test_51OFx9tAxKaki6GC7GG6ez6voYw1Yr6MRVTCmb2nwRKkI4zkFnHYLmBkk5nUBU3qMpVqz1A5018ANDg5MzPzUVh2j00VJk2GRwj');
-?>
